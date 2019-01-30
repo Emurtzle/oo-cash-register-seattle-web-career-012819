bf06bef688dda26584364ca0aefc9d21a5981300
Emily Wagner <emilywagner598@gmail.com>
2019-01-30 18:31:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
     if self.discount > 0
       #binding.pry
       self.total = self.total - (self.total * (self.discount/100))
-      return "After the discount, the total comes to $#{self.total}"
+      return "After the discount, the total comes to $#{self.total.to_i}"
     else
       return "There is no discount to apply."
     end
