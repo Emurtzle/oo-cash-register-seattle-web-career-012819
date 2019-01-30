a28d705ecb1a1c0fb1f0046daf52ee8bf67ecf46
Emily Wagner <emilywagner598@gmail.com>
2019-01-30 18:23:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,6 +20,7 @@ class CashRegister
 
   def apply_discount
     if self.discount > 0
+      binding.pry
       self.total = self.total * (self.discount/100)
       return "After the discount, the total comes to $#{self.total}"
     else
